1c7e136d413002ebad16e10788aca8405135c3c1
Dmitriy Dultsev <tsukikohaya@gmail.com>
2023-08-09 12:22:01 +0300

    fixed images in config and create sidebars

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { User } from "@prisma/client";
+import Image from "next/image";
+
+interface AvatarProps {
+   user?: User;
+}
+
+const Avatar = ({ user }: AvatarProps) => {
+   return (
+      <div className="relative">
+         <div
+            className="
+            relative
+            inline-block
+            rounded-full
+            overflow-hidden
+            h-9
+            w-9
+            md:h-11
+            md:w-11
+         "
+         >
+            <Image
+               fill
+               alt="Avatar"
+               src={user?.image || "/images/placeholder.jpg"}
+            />
+         </div>
+      </div>
+   );
+};
+
+export default Avatar;
--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,7 +1,87 @@
-import React from 'react'
+"use client";
 
-export default function DesktopSidebar() {
-  return (
-    <div>DesktopSidebar</div>
-  )
+import useRoutes from "@/app/hooks/useRoutes";
+import React, { useState } from "react";
+import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
+
+interface Props {
+   currentUser: User;
+}
+
+export default function DesktopSidebar({
+   currentUser,
+}: Props) {
+   const routes = useRoutes();
+   const [isOpen, setIsOpen] = useState(false);
+
+   console.log(currentUser);
+
+   return (
+      <div
+         className="
+          hidden
+          lg:fixed
+          lg:inset-y-0
+          lg:left-0
+          lg:z-40
+          lg:w-20
+          xl:px-6
+          lg:overflow-y-auto
+          lg:bg-white
+          lg:border-r-[1px]
+          lg:pb-4
+          lg:flex
+          lg:flex-col
+          justify-between
+    "
+      >
+         <nav className="mt-4 flex flex-col justify-between">
+            <ul
+               role="list"
+               className="flex flex-col items-center space-y-1"
+            >
+               {routes.map((item) => (
+                  <DesktopItem
+                     key={item.label}
+                     href={item.href}
+                     label={item.label}
+                     icon={item.icon}
+                     active={item.active}
+                     onClick={item.onClick}
+                  />
+               ))}
+            </ul>
+         </nav>
+         <nav className="mt-4 flex flex-col justify-between items-center">
+            <div
+               onClick={() => setIsOpen(true)}
+               className="
+                cursor-pointer
+                hover:opacity-75  
+                transition
+            "
+            >
+               <Avatar user={currentUser} />
+            </div>
+            <span
+               className="
+               absolute
+               block
+               rounded-full
+               bg-green-500
+               ring-2
+               ring-white
+               top-0
+               right-0
+               h-2
+               w-2
+               md:h-3
+               md:w-3
+            "
+            ></span>
+         </nav>
+      </div>
+   );
 }
